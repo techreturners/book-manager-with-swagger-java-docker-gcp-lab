232c98661fdbd7c4c70ceaf42f7f0014b4a53f67
James Heggs <james.heggs@techreturners.com>
2021-10-26 19:03:12 +0100

    Initial repo creation, templated from previous app

---
+++ README.md
@@ -0,0 +1,34 @@
+# 📖 Minimalist Book Manager API
+
+## Introduction
+This is a starter repository for the Minimalist Book Manager API demonstrating basic use of SpringFox and SpringFox Swagger UI
+to produce Swagger API documentation.
+
+### Pre-Requisites
+- Java SE Development Kit 11
+- Maven
+
+### Technologies & Dependencies
+- Spring Boot
+- Spring Web
+- SpringFox
+- springfox-swagger-ui
+- H2 Database
+- mysql-connector-java
+- Lombok
+- Spring Data JPA
+
+### How to Get Started
+- Fork this repo to your Github and then clone the forked version of this repo
+
+### Main Entry Point
+- The Main Entry Point for the application is: [BookmanagerApplication.java](src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java)
+
+### Running the Unit Tests
+- You can run the unit tests in IntelliJ, or you can go to your terminal and inside the root of this directory, run:
+
+`mvn test`
+
+### Externalising Configuration - application.properties and Spring Profiles
+
+- An example structure for Spring Profiles can be viewed in the [resources directory](src/main/resources).
---
+++ src/main/java/com/techreturners/bookmanager/service/BookManagerServiceImpl.java
@@ -0,0 +1,55 @@
+package com.techreturners.bookmanager.service;
+
+import com.techreturners.bookmanager.model.Book;
+import com.techreturners.bookmanager.repository.BookManagerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BookManagerServiceImpl implements BookManagerService {
+
+    BookManagerRepository bookManagerRepository;
+
+    public BookManagerServiceImpl(BookManagerRepository bookManagerRepository) {
+        this.bookManagerRepository = bookManagerRepository;
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        List<Book> books = new ArrayList<>();
+        bookManagerRepository.findAll().forEach(books::add);
+        return books;
+    }
+
+    @Override
+    public Book insertBook(Book book) {
+        return bookManagerRepository.save(book);
+    }
+
+    @Override
+    public Book getBookById(Long id) {
+        return bookManagerRepository.findById(id).get();
+    }
+
+    //User Story 4 - Update Book By Id Solution
+    @Override
+    public void updateBookById(Long id, Book book) {
+        Book retrievedBook = bookManagerRepository.findById(id).get();
+
+        retrievedBook.setTitle(book.getTitle());
+        retrievedBook.setDescription(book.getDescription());
+        retrievedBook.setAuthor(book.getAuthor());
+        retrievedBook.setGenre(book.getGenre());
+
+        bookManagerRepository.save(retrievedBook);
+    }
+
+    //User Story 5 - Delete Book by Id Solution
+    public void deleteBookById(Long id) {
+        Book retrievedBook = bookManagerRepository.findById(id).get();
+        bookManagerRepository.delete(retrievedBook);
+    }
+
+}
