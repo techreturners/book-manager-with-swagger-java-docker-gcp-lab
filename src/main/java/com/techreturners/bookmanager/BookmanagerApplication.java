223124fc256278d637f50d8daf292a74671115e7
Simizzle <85163420+Simizzle@users.noreply.github.com>
2023-09-08 11:25:48 +0100

    fix: updates springfox with springdocs
    adds an OpenAPI custom security scheme and Info()

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -1,40 +1,34 @@
 package com.techreturners.bookmanager;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BookmanagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookmanagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookmanagerApplication.class, args);
+    }
 
-	@Bean
-	public Docket swaggerConfiguration() {
-		return new Docket(DocumentationType.OAS_30)
-				.select()
-				.paths(PathSelectors.any())
-				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-				.build()
-				.apiInfo(apiInformation());
-	}
-
-	private ApiInfo apiInformation() {
-		return new ApiInfoBuilder()
-				.title("📖 Minimalist Book Manager API")
-				.description("Need a helping hand to manage your books 😺? " +
-						"Well, you're in luck! This API will let you manage your books with ease! 😻")
-				.version("v1")
-				.build();
-	}
+    @Bean
+    public OpenAPI customOpenAPI(@Value("v1") String appVersion) {
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("basicScheme",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info()
+                        .title("📖 Minimalist Book Manager API")
+                        .version(appVersion)
+                        .description("Need a helping hand to manage your books 😺? " +
+                                "Well, you're in luck! This API will let you manage your books with ease! 😻")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 
 }
